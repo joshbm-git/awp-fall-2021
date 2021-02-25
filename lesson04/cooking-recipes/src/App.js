8ba2802fb656826586b91606821243e3a8f84a61
Kristian Dorland <kristian@dorland.dk>
2021-02-25 11:03:28 +0100

    Adding example of fetching data, but commenting it out by default.

--- lesson04/cooking-recipes/src/App.js
+++ lesson04/cooking-recipes/src/App.js
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Router } from "@reach/router";
 
 import Recipes from "./Recipes";
 import Recipe from "./Recipe";
 
+const data = [
+  { id: 1, title: 'Pizza 1', description: "Pizza is nice 1" },
+  { id: 2, title: 'Pizza 2', description: "Pizza is nice 2" },
+  { id: 3, title: 'Pizza 3', description: "Pizza is nice 3" },
+];
+
 function App() {
-  const [recipes, setRecipes] = useState([
-    { id: 1, title: 'Pizza 1', description: "Pizza is nice 1" },
-    { id: 2, title: 'Pizza 2', description: "Pizza is nice 2" },
-    { id: 3, title: 'Pizza 3', description: "Pizza is nice 3" },
-  ]);
+  const [recipes, setRecipes] = useState(data);
+  //const [recipes, setRecipes] = useState([]);
+/*
+  useEffect(() => { 
+    const fetchData = async () => {
+      const url = "https://krdo-cooking-api.herokuapp.com/api/cooking";
+      const response = await fetch(url);
+      const data = await response.json();
+      setRecipes(data);
+    }; 
+    fetchData();
+  }, []); 
+  */
 
   function getRecipe(id) {
     return recipes.find(recipe => recipe.id === parseInt(id));
