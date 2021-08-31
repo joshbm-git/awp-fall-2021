f1ea38d683c2f7ea662d15f955442a07235dadae
Dan Okkels Brendstrup <brendstrup@gmail.com>
2021-08-31 21:46:39 +0200

    feat: small tweaks to the task list template

--- lesson02/react-task-list-template/src/App.js
+++ lesson02/react-task-list-template/src/App.js
@@ -1,25 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
   // This is your task state
-  const [ data, setData ] = useState([
-    { task: 'Do laundry', done: false },
-    { task: 'Clean bedroom', done: true },
-    { task: 'Bake cake', done: true }
+  const [data, setData] = useState([
+    { task: "Do laundry", done: false },
+    { task: "Clean bedroom", done: true },
+    { task: "Bake cake", done: true },
   ]);
-
-  // TODO: Use a "for loop" and put each task inside <li></li>, then put each <li></li> inside this array.
-  const array = [];
 
   return (
     <>
       <h2>Task List</h2>
       <ul>
-        {/* The array of <li> elements are rendered here: */}
-        {array} 
+        {/* TODO: Map over the `data` array to render the items as <li>'s here */}
       </ul>
     </>
-  )
+  );
 }
 
 export default App;
